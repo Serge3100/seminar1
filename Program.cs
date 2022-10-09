2c07f0bae5ed09eb5642422e3ce4cefd9cfd0535
Sergey <serge3100@mail.ru>
2022-10-09 18:58:03 +0300

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 0;
+int b = 0;
+
+Console.WriteLine("Введите число a:");
+
+a = Convert.ToInt32(Console.ReadLine()) ;
+Console.WriteLine("Введите число b:");
+
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+     Console.WriteLine($"число {a} больше");
+}
+else Console.WriteLine($"число {b} больше");
